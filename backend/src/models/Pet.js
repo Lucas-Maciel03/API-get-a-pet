f1211b75d6c0d2158be4814d67402fef3fc3abb9
Lucas Maciel <farias.maciel@outlook.com>
2023-10-01 22:10:18 -0400

    criando model de Pet

---
+++ backend/src/models/Pet.js
@@ -0,0 +1,38 @@
+const mongoose = require('../db/conn')
+const { Schema } = mongoose
+
+const Pet = mongoose.model(
+    'Pet',
+    new Schema({
+        name: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true
+        },
+        description: {
+            type: String,
+        },
+        weight: {
+            type: Number,
+            required: true
+        },
+        color: {
+            type: String,
+            required: true
+        },
+        images: {
+            type: Array,
+            required: true
+        },
+        available: {
+            type: Boolean
+        },
+        user: Object, //armazenar informações do dono
+        adopter: Object //armazenar informações do adotante
+    }, { timestamps: true })
+)
+
+module.exports = Pet
